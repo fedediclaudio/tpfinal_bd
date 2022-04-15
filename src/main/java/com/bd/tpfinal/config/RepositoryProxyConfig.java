e3a478357ea0ae3a81785c157c011bfa8b0f5e09
Pablo Fernandez <pmfernan@gmail.com>
2022-04-15 20:41:01 -0300

    Created beans for repositories

--- src/main/java/com/bd/tpfinal/config/RepositoryProxyConfig.java
+++ src/main/java/com/bd/tpfinal/config/RepositoryProxyConfig.java
@@ -3,13 +3,17 @@ package com.bd.tpfinal.config;
 import com.bd.tpfinal.mappers.client.ClientMapper;
 import com.bd.tpfinal.mappers.item.ItemMapper;
 import com.bd.tpfinal.mappers.orders.OrderMapper;
+import com.bd.tpfinal.mappers.product.ProductMapper;
+import com.bd.tpfinal.mappers.suppplier.SupplierMapper;
 import com.bd.tpfinal.proxy.repositories.ClientRepositoryProxy;
 import com.bd.tpfinal.proxy.repositories.OrderRepositoryProxy;
+import com.bd.tpfinal.proxy.repositories.ProductRepositoryProxy;
+import com.bd.tpfinal.proxy.repositories.SupplierRepositoryProxy;
 import com.bd.tpfinal.proxy.repositories.impl.ClientRepositoryProxyImpl;
 import com.bd.tpfinal.proxy.repositories.impl.OrderRepositoryProxyImpl;
-import com.bd.tpfinal.repositories.ClientRepository;
-import com.bd.tpfinal.repositories.OrderRepository;
-import com.bd.tpfinal.repositories.ProductRepository;
+import com.bd.tpfinal.proxy.repositories.impl.ProductRepositoryProxyImpl;
+import com.bd.tpfinal.proxy.repositories.impl.SupplierRepositoryProxyImpl;
+import com.bd.tpfinal.repositories.*;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -21,14 +25,34 @@ public class RepositoryProxyConfig {
                                                         OrderRepository orderRepository,
                                                         ClientRepository clientRepository,
                                                         ProductRepository productRepository,
+                                                        DeliveryManRepository deliveryManRepository,
+                                                        SupplierRepository supplierRepository,
                                                         ClientMapper clientMapper,
                                                         ItemMapper itemMapper) {
-        return new OrderRepositoryProxyImpl(orderMapper, orderRepository, clientRepository, productRepository, clientMapper, itemMapper);
+        return new OrderRepositoryProxyImpl(orderMapper, orderRepository, clientRepository, productRepository, deliveryManRepository, supplierRepository, clientMapper, itemMapper);
 
     }
 
     @Bean
     public ClientRepositoryProxy getClientRepositoryProxy(ClientRepository clientRepository, ClientMapper clientMapper){
         return new ClientRepositoryProxyImpl(clientRepository, clientMapper);
+    }
+
+    @Bean
+    public ProductRepositoryProxy getProductRespositoryProxy(ProductRepository productRepository,
+                                                             HistoricalProductPriceRepository historicalProductPriceRepository,
+                                                             ProductMapper productMapper){
+        return new ProductRepositoryProxyImpl(productRepository, historicalProductPriceRepository, productMapper);
+    }
+
+    @Bean
+    public SupplierRepositoryProxy getSupplierRepositoryProxy(SupplierRepository supplierRepository,
+                                                              SupplierMapper supplierMapper,
+                                                              ProductRepository productRepository,
+                                                              ProductTypeRepository productTypeRepository,
+                                                              ProductMapper productMapper,
+                                                              SupplierWithOrdersCountRepository supplierWithOrdersCountRepository){
+        return new SupplierRepositoryProxyImpl(supplierRepository, productTypeRepository, productRepository, supplierMapper,
+                productMapper, supplierWithOrdersCountRepository);
     }
 }
