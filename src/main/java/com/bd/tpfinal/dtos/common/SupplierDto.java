08d69791203ca08987d5627270b2051501f1f9ae
Pablo Fernandez <pmfernan@gmail.com>
2022-05-22 00:03:05 -0300

    Updated dtos

--- src/main/java/com/bd/tpfinal/dtos/common/SupplierDto.java
+++ src/main/java/com/bd/tpfinal/dtos/common/SupplierDto.java
@@ -1,15 +1,12 @@
 package com.bd.tpfinal.dtos.common;
 
-import com.bd.tpfinal.model.Product;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
-import lombok.Builder;
 import lombok.Data;
 
 import java.util.List;
 @Data
-@AllArgsConstructor
 @JsonInclude(JsonInclude.Include.NON_EMPTY)
 public class SupplierDto {
 
@@ -21,10 +18,25 @@ public class SupplierDto {
     private String address;
     @JsonProperty("qualification_of_users")
     private float qualificationOfUsers;
+    @JsonProperty("one_star_qualification_count")
+    private float oneStarQualificationCount;
     List<ProductDto> products;
     @JsonProperty("supplier_type_id")
     String supplierTypeId;
     @JsonProperty("supplier_type")
     String supplierType;
 
+    public SupplierDto(String supplierId, String name, String cuil, float[] coords, String address,
+                       float qualificationOfUsers, List<ProductDto> products,
+                       String supplierTypeId, String supplierType) {
+        this.supplierId = supplierId;
+        this.name = name;
+        this.cuil = cuil;
+        this.coords = coords;
+        this.address = address;
+        this.qualificationOfUsers = qualificationOfUsers;
+        this.products = products;
+        this.supplierTypeId = supplierTypeId;
+        this.supplierType = supplierType;
+    }
 }
--- src/main/java/com/bd/tpfinal/dtos/common/UserDto.java
+++ src/main/java/com/bd/tpfinal/dtos/common/UserDto.java
@@ -1,5 +1,6 @@
 package com.bd.tpfinal.dtos.common;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 @Data
@@ -8,6 +9,7 @@ public abstract class UserDto {
     private String id;
     private String name;
     private String username;
+    @JsonIgnore
     private String password;
     private String email;
 
