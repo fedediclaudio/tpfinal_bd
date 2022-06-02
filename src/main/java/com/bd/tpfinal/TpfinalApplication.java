a9d2eb433e699cfdd312dbdb240482fdb71988c1
unknown <amandifederico@gmail.com>
2022-06-01 23:16:15 -0300

    test conexion

--- src/main/java/com/bd/tpfinal/TpfinalApplication.java
+++ src/main/java/com/bd/tpfinal/TpfinalApplication.java
@@ -6,18 +6,20 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 @SpringBootApplication
+@EnableMongoRepositories
 public class TpfinalApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(TpfinalApplication.class, args);
 	}
 	@Bean
-	CommandLineRunner runner(UserRepository repository){
+	CommandLineRunner runner(UserRepository userRepository){
 		return args -> {
 			User user = new User();
-			repository.insert(user);
+			userRepository.save(user);
 		};
 	}
 
