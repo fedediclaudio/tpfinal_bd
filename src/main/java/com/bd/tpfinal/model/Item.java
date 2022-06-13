cd97c990bc411edf502fde8e53438a33bee4ab3d
mauro89x <mauro.a89xdev@gmail.com>
2022-06-13 18:06:52 -0300

    Update pom.xml

--- src/main/java/com/bd/tpfinal/model/Item.java
+++ src/main/java/com/bd/tpfinal/model/Item.java
@@ -5,7 +5,6 @@ import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.DBRef;
 import org.springframework.data.mongodb.core.mapping.DocumentReference;
 
-
 @Data
 public class Item {
 
